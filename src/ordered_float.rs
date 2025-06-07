6f32893d064d315ed36bb4a548f58a10617594f0
nnethercott <nathaniel@deepomatic.com>
2025-06-07 16:42:06 +0200

    doc string

--- src/ordered_float.rs
+++ src/ordered_float.rs
@@ -1,32 +1,31 @@
 /// A wrapper type implementing `Ord`
-/// f32s are not ordered due to NaN bit patterns but we need some way of ranking leaf nodes in
-/// arroy based off distance to a query
 ///
 /// Since distance metrics satisfy d(x,x)=0 and d(x,y)>0 for x!=y we don't need to operate on the
 /// full range of an f32. Comparing the u32 representation of a non-negative f32 should suffice and
 /// is actually a lot quicker.
+///
+/// We just need to ensure -0.0's and NaN's don't show up anywhere cause f32<Nan<-0
 #[derive(Clone)]
 pub struct NonNegativeOrderedFloat(pub f32);
 
-impl PartialEq for NonNegativeOrderedFloat{
+impl PartialEq for NonNegativeOrderedFloat {
     fn eq(&self, other: &Self) -> bool {
         self.0.to_bits().eq(&other.0.to_bits())
     }
 }
 
-impl Eq for NonNegativeOrderedFloat{}
+impl Eq for NonNegativeOrderedFloat {}
 
-impl PartialOrd for NonNegativeOrderedFloat{
+impl PartialOrd for NonNegativeOrderedFloat {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         self.0.to_bits().partial_cmp(&other.0.to_bits())
     }
 }
 
-impl Ord for NonNegativeOrderedFloat{
+impl Ord for NonNegativeOrderedFloat {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.0.to_bits().cmp(&other.0.to_bits())
     }
 }
-
 
 // TODO: add proptests and unit tests here
